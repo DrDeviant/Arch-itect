21ffbcdfad41ad3d2455b160f50454f96db1976b
Athi Jenakumran <athi@arch.com>
2021-12-22 20:57:57 +0000

    added the backup script, and ignorelist ⭐ :)

---
+++ backfullup.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+ 
+# a forward slash, means copy directory contents only
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+#cat $DIR/backupIgnoreList.txt
+#exit
+# The source path to backup. Can be local or remote.
+SOURCE=/home/rdkang
+# Where to store the incremental backups
+DESTBASE=pi:/mnt/sambacker/backedmeup
+ 
+# Where to store today's backup
+DEST="$DESTBASE/$(date +%Y-%m-%d)"
+# Where to find yesterday's backup
+YESTERDAY="$DESTBASE/$(date -d yesterday +%Y-%m-%d)/"
+ 
+# Use yesterday's backup as the incremental base if it exists
+if [ -d "$YESTERDAY" ]
+then
+	OPTS="--link-dest $YESTERDAY"
+fi
+ 
+# Run the rsync
+rsync -avAX -hP --exclude-from=/home/rdkang/Documents/backupIgnoreList.txt --exclude '*.iso' --exclude '*lecture0-1080p*' "$SOURCE" "$DEST" 
+echo "backing up /etc"
+rsync -avAX -hP  "/etc" "$DEST" 
