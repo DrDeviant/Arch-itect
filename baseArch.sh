8be4f6a3e2ed7cfa4165390bfe179e6083901118
Athi Jenakumran <athi@arch.com>
2022-01-04 16:37:43 +0000

    bash script for the base arch install

---
+++ baseArch.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  loadkeys uk
+  setfont /usr/share/kbd/consolefonts/ter-v28b.psf.gz
+
+  timedatectl set-ntp true
+
+  echo "time to start partioning drives with `fdisk`"
+  exit
+
+else
+  if [[ $1 =~ (part2) ]]; then
+    mkfs.ext4 /dev/sda1
+    # root
+    mkfs.ext4 /dev/sda3
+    # home
+    mkfs.ext4 /dev/sda4
+
+    # swap
+    mkswap /dev/sda2
+    swapon /dev/sda2
+
+    ## Mounting
+    mkdir /mnt/home /mkdir/boot
+    mount /dev/sda3 /mnt
+    mkdir /mnt/boot
+    mount /dev/sda1 /mnt/boot
+    mkdir /mnt/home
+    mount /dev/sda4 /mnt/home
+
+    pacstrap /mnt base base-devel linux linux-firmware vim neovim
+
+
+    genfstab -U /mnt >> /mnt/etc/fstab
+
+    echo "use 'arch-chroot /mnt' then run './sh part3chroot'"
+
+else
+  if [[ $1 =~ (part3chroot) ]]; then
+      pacman -S networkmanager grub
+      systemctl enable NetworkManager
+
+      grub-install /dev/sda
+      grub-mkconfig -o /boot/grub/grub.cfg
+
+      passwd
+
+      # uncomment out en_GB in file
+      sed -i '/en_GB/s/^#//g' /etc/locale.gen
+      locale-gen
+
+      ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+      hwclock --systohc
+
+      echo "archthinkpad" > /etc/hostname
+      echo "LANG=en_US.UTF-8" > /etc/locale.conf
