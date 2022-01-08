cad38e1bcbbe18f58aa0e0ebf23fb54a962542db
Athi Jenakumran <athi@arch.com>
2022-01-08 17:28:53 +0000

    automated git commit

--- GitSetter.sh
+++ GitSetter.sh
@@ -10,6 +10,9 @@ git clone git@github.com:rdkang/Arch-itect
 git clone git@github.com:rdkang/cs50
 
 cd
+cd ~/.local/bin/ ; git clone git@github.com:rdkang/supersecretscripts ; mv supersecretscripts sss
+cd
+
 
 
 # for the main dotfiles
