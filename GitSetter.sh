92e9de0e017c90540997d153d1a1feb68ee89129
Athi Jenakumran <athi@arch.com>
2022-01-08 10:55:48 +0000

    automated git commit

--- GitSetter.sh
+++ GitSetter.sh
@@ -10,3 +10,30 @@ git clone git@github.com:rdkang/Arch-itect
 git clone git@github.com:rdkang/cs50
 
 cd
+
+
+git branch -m master  main
+
+
+echo '
+[core]
+  repositoryformatversion = 0
+  filemode = true
+  bare = false
+  logallrefupdates = true
+[submodule]
+  active = .
+[remote "origin"]
+  url = git@github.com:Rdkang/dotfiles.git
+  fetch = +refs/heads/main:refs/remotes/origin/main
+[branch "main"]
+  remote = origin
+  merge = refs/heads/main
+[submodule ".config/mpv/script_modules/mpvSockets"]
+  url = https://github.com/wis/mpvSockets.git
+[status]
+  showUntrackedFiles = no
+' > ~/.git/config
+
+git fetch
+git pull -f
